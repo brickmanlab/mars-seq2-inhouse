407512810fdc7840355b607727dd46031361db8f
Martin Proks <mproksik@gmail.com>
2021-05-18 12:07:16 +0200

    fix: whitelist build per batch
    Previously the whitelist was build for the whole dataset.
    With the current setup of the pipeline, we need to build it
    per Batch instead.

--- {{cookiecutter.project_dir}}/helpers/build_whitelist.py
+++ {{cookiecutter.project_dir}}/helpers/build_whitelist.py
@@ -1,6 +1,5 @@
 #!/user/bin/env python3
 # pip install openpyxl
-import os
 import glob
 import argparse
 import pandas as pd
@@ -18,15 +17,16 @@ if __name__ == "__main__":
     # whitelist = metadata.loc[metadata['Batch'] == args.batch, ['Pool_barcode', 'Cell_barcode']].agg(''.join, axis=1).str.strip()
     # whitelist.to_csv('./whitelist.txt', header=None, index=None)
 
-    batches = pd.read_excel(glob.glob(f'{args.input}/amp*.xlsx')[0])
+    batches = pd.read_excel(glob.glob(f'{args.input}/amp*.xlsx',)[0])
     wells = pd.read_excel(glob.glob(f'{args.input}/wells*.xlsx')[0])
 
-    batches = batches[['Seq_batch_ID', 'Pool_barcode']].sort_values(by='Seq_batch_ID')
-    wells = wells[['Amp_batch_ID', 'Cell_barcode']].sort_values(by='Amp_batch_ID')
+    batches = batches[batches.Seq_batch_ID == args.batch]
+    wells = wells[wells.Amp_batch_ID.isin(batches.Amp_batch_ID)]
+    wells = wells[['Amp_batch_ID', 'Cell_barcode']]
 
     batches = batches.astype('category')
     wells = wells.astype('category')
-    
+
     wells['Amp_batch_ID'] = wells['Amp_batch_ID'].cat.rename_categories(batches.Pool_barcode.unique())
     wells['whitelist'] = wells[['Amp_batch_ID', 'Cell_barcode']].agg(''.join, axis=1)
 
