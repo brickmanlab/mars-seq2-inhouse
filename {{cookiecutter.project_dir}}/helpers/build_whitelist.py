f49a5617783f6259994666e101c4aa7a0fbf4f83
Martin Proks <mproksik@gmail.com>
2021-05-19 16:39:46 +0200

    fix: empty spaces in metadata

--- {{cookiecutter.project_dir}}/helpers/build_whitelist.py
+++ {{cookiecutter.project_dir}}/helpers/build_whitelist.py
@@ -20,14 +20,16 @@ if __name__ == "__main__":
     batches = pd.read_excel(glob.glob(f'{args.input}/amp*.xlsx',)[0])
     wells = pd.read_excel(glob.glob(f'{args.input}/wells*.xlsx')[0])
 
-    batches = batches[batches.Seq_batch_ID == args.batch]
+    # preprocess and clean-up
+    batches = batches[batches.Seq_batch_ID == args.batch].astype('category')
     wells = wells[wells.Amp_batch_ID.isin(batches.Amp_batch_ID)]
-    wells = wells[['Amp_batch_ID', 'Cell_barcode']]
+    wells = wells[['Amp_batch_ID', 'Cell_barcode']].str.strip()
+    wells['Amp_batch_ID'] = wells['Amp_batch_ID'].str.strip().astype('category')
+    wells['Cell_barcode'] = wells['Cell_barcode'].str.strip().astype('category')
 
-    batches = batches.astype('category')
-    wells = wells.astype('category')
-
+    # merge and concat barcodes
     wells['Amp_batch_ID'] = wells['Amp_batch_ID'].cat.rename_categories(batches.Pool_barcode.unique())
     wells['whitelist'] = wells[['Amp_batch_ID', 'Cell_barcode']].agg(''.join, axis=1)
 
+    # save
     wells['whitelist'].to_csv('whitelist.txt', header=None, index=None)
